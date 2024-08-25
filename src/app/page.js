844f3e1a8799fb79d88951b3e5b9e2978ef0ca03
Reni Christian <renisonreni@gmail.com>
2024-08-25 20:32:19 +0530

    deployment testing 1

--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,12 @@ import FeaturedArtists from "@/components/new/FeaturedArtists";
 
 export default async function Home() {
 
-  // const supabase = createServerComponentClient({ cookies })
-  // const { data: langData, error: langError } = await supabase.from('language').select('*')
-  // const { data: songData, error: songError } = await supabase.from('song').select('*')
-  // const { data: artistData, error: artistError } = await supabase.from('artist').select('*, song_artists(*)')
+  const supabase = createServerComponentClient({ cookies })
+  const { data: langData, error: langError } = await supabase.from('language').select('*')
+  const { data: songData, error: songError } = await supabase.from('song').select('*')
+  const { data: artistData, error: artistError } = await supabase.from('artist').select('*, song_artists(*)')
 
-  // console.log(artistData);
+  console.log(artistData);
 
   return (
     <div>
